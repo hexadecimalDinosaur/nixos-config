d14987c2f56022602bb5d77db3f0cb80f78fd4ef
Ivy Fan-Chiang <dev@ivyfanchiang.ca>
2025-01-28 01:39:09 -0500

    move home-manager private to secrets

--- home-manager/ssh.nix
+++ home-manager/ssh.nix
@@ -1,14 +1,14 @@
 { ... }:
 {
+  imports = [
+    ./secrets/ssh-hosts.nix
+  ];
+
   programs.ssh = {
     enable = true;
     addKeysToAgent = "yes";
     forwardAgent = true;
     controlMaster = "auto";
     controlPersist = "5m";
-
-    matchBlocks = {
-      # REDACTED
-    };
   };
 }
