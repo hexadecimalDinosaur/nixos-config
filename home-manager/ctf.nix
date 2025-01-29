aab77887eb83f530dd72f239d57db5833e312a49
Ivy Fan-Chiang <dev@ivyfanchiang.ca>
2025-01-29 01:24:16 -0500

    ctf/pentesting/rev tools

---
+++ home-manager/ctf.nix
@@ -0,0 +1,153 @@
+{ pkgs, config, libs, ... }:
+let
+  unstable = import <nixpkgs-unstable> { };
+
+  # unstable-pkgs = with unstable; [
+  # ];
+
+  rev = with pkgs; [
+    # static analysis
+    # ===================
+    radare2
+    cfr
+    # jadx
+    upx
+    (ghidra.withExtensions (
+      p: with p; [
+        ghidra-golanganalyzerextension
+        sleighdevtools
+        gnudisassembler
+        machinelearning
+        findcrypt
+      ]))
+    detect-it-easy
+    # malware analysis
+    # ===================
+    yara
+    # yara-x
+  ];
+  forensics = with pkgs; [
+    # forensics
+    # ===================
+    volatility3
+    wireshark
+    exiftool
+    termshark
+    binwalk
+    imhex
+    evtx
+    # autopsy
+    imagemagick
+    tcpdump
+    ngrep
+
+    # osint
+    # ===================
+    uncover
+    # theharvester
+
+    # steganography
+    # ===================
+    sherlock
+    stegsolve
+    stegseek
+    steghide
+    zsteg
+  ];
+  web = with pkgs; [
+    sqlmap
+    burpsuite
+    ghauri
+  ];
+  pwn = with pkgs; [
+    pwninit
+    pwndbg
+    gef
+  ];
+  redteaming = with pkgs; [
+    # scanning - network
+    # ===================
+    nmap
+    nmap-formatter
+    nmap-parse
+    rustscan
+    das
+    netdiscover
+    massdns
+    # masscan
+    # p0f
+
+    # scanning - web
+    # ===================
+    nikto
+    zgrab2
+
+    # scanning - linux
+    # ===================
+    vulnix
+    # linux-exploit-suggester
+    # data collection
+    # responder
+    # exploitation
+    metasploit
+    pacu
+
+    # brute force attacks
+    # ===================
+    john
+    # brutespray
+    # thc-hydra
+    hashcat
+    crunch
+    # dirbuster
+    # gobuster
+  ];
+  misc = with pkgs; [
+    pwncat
+    wordlists
+  ];
+  custom = [
+    (import ./custom-pkgs/vol2.nix)
+  ];
+in
+{
+  py3Pkgs = ps: with ps; [
+    # forensics
+    # ===================
+    # dissect
+    evtx
+    # pwn
+    pwntools
+    ropgadget
+
+    # scanning
+    # ===================
+    python-nmap
+    nmapthon2
+    impacket
+
+    # web
+    # ===================
+    mitmproxy
+
+    # intel
+    # ===================
+    shodan
+    censys
+
+    # analysis
+    # ===================
+    yara-python
+  ];
+
+  home.packages = builtins.concatLists [
+    # unstable-pkgs
+    rev
+    web
+    pwn
+    forensics
+    redteaming
+    misc
+    custom
+  ];
+}
