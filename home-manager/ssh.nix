cd6c0b8849c4d2c0147bacbee9ad4c79d7877c29
Ivy Fan-Chiang <dev@ivyfanchiang.ca>
2025-02-04 10:08:45 -0500

    update ssh config

--- home-manager/ssh.nix
+++ home-manager/ssh.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   imports = [
     ./secrets/ssh-hosts.nix
@@ -10,5 +10,8 @@
     forwardAgent = true;
     controlMaster = "auto";
     controlPersist = "5m";
+    extraConfig = ''
+      PKCS11Provider ${pkgs.tpm2-pkcs11}/lib/libtpm2_pkcs11.so
+    '';
   };
 }
