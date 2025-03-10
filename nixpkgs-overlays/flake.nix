7c8a0826170e9277220c02033123097dd18bbb5f
Ivy Fan-Chiang <dev@ivyfanchiang.ca>
2025-03-10 13:55:23 -0400

    add overlays

---
+++ nixpkgs-overlays/flake.nix
@@ -0,0 +1,13 @@
+{
+  inputs = {
+    nixpkgs-unstable.url = "flake:nixpkgs-unstable";
+  };
+  outputs = { nixpkgs-unstable, ... } : {
+    overlays = rec {
+      plasma = import ./plasma.nix { };
+      unstable = import ./unstable.nix { inherit nixpkgs-unstable; };
+
+      default = unstable;
+    };
+  };
+}
